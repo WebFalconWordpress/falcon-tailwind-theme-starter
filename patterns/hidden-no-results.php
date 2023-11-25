64463858f1951a432c48d6f197a7e554990dd2ed
Danilo Puric <danilo@bild-studio.net>
2023-11-25 16:04:28 +0100

    This is first version of Falcon Tailwind WordrPess Block Theme! BRAVOgit add .!

---
+++ patterns/hidden-no-results.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: No results
+ * Slug: pubcalls/hidden-no-results
+ * Inserter: no
+ */
+?>
+<!-- wp:paragraph -->
+<p><?php echo esc_html_x( 'No posts were found.', 'Message explaining that there are no results returned from a search', 'pubcalls' ); ?></p>
+<!-- /wp:paragraph -->
